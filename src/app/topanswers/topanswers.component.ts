d3a22256929fa8b9cc8ffe80449ea8bc9d180caa
mmuniz75 <muniz_marcelo@hotmail.com>
2017-11-29 18:28:47 -0200

    icheck to topanswers

--- src/app/topanswers/topanswers.component.ts
+++ src/app/topanswers/topanswers.component.ts
@@ -4,6 +4,7 @@ import { IAnswer } from '../../model/Answer';
 import { AnswerService } from '../../services/answer.service';
 import { MessageService } from '../../services/message.service';
 
+declare var $: any;
 
 @Component({
   selector: 'app-topanswers',
@@ -14,9 +15,30 @@ export class TopanswersComponent implements OnInit {
   constructor(private answerService : AnswerService) { }
   
     answers:IAnswer[];
+    feedback : boolean = false;
       
     ngOnInit() {
+      $.getScript("../../assets/js/icheck.min.js");
+      $.getScript("../../assets/js/custom.min.js");
       this.loadTopAnswers(false);
+    }
+    
+    ngAfterViewChecked(){
+      let self = this;
+      $('input').on('ifChecked', function(event){
+        if(!self.feedback){
+          self.feedback = true;
+          self.loadTopAnswers(true);
+        }
+      });
+
+      $('input').on('ifUnchecked', function(event){
+        if(self.feedback){
+          self.feedback = false;
+          self.loadTopAnswers(false);
+        }
+      });
+      
     }
 
     loadTopAnswers(feedback){
