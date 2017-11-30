a0c03e5af9493f3bea45d394fd0f20299d67e7e8
mmuniz75 <muniz_marcelo@hotmail.com>
2017-11-30 16:49:46 -0200

    icone and server set

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  SERVER_URL : "",
+  SERVER_URL : "server-admin-askalien-b.193b.starter-ca-central-1.openshiftapps.com",
   SERVER_CONF : "https://raw.githubusercontent.com/mmuniz75/askalien-admin/master/server.properties"
 };
